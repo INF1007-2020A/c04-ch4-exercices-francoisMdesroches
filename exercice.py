1eb7b431d87f98d29e42b2d260246295c917236e
francoisMdesroches <f.deroche@hotmail.com>
2020-09-15 22:45:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,7 +10,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    return string[]
+    return string[0, 2]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
