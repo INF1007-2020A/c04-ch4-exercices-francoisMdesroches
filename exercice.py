9adb51c439bdcd83b166813ada15419610f5edb4
francoisMdesroches <f.deroche@hotmail.com>
2020-09-15 23:09:35 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -34,7 +34,8 @@ def get_number_of_char(string: str, char: str) -> int:
 def get_number_of_words(sentence: str, word: str) -> int:
     nb_mots = 0
 
-    for i in range(len(string))
+    for i in range(len(string)-4)
+        if()
 
     return nb_mots
 
