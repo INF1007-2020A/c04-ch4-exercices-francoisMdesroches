5d95214f08004639e83c9dce2655a39356e5de2a
francoisMdesroches <f.deroche@hotmail.com>
2020-09-15 23:08:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -33,8 +33,9 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     nb_mots = 0
-    
-    pass
+
+
+    return nb_mots
 
 
 def main() -> None:
