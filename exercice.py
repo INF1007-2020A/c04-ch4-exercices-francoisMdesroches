cbef2288be71b43b7e4c006e47acb69da81e872e
francoisMdesroches <f.deroche@hotmail.com>
2020-09-15 23:04:13 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,6 +17,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
     if(old_char in string):
         for i in range(len(string)-1):
             if string[i] == old_char:
+                string = 
                 string[i] = new_char
     return string
 
