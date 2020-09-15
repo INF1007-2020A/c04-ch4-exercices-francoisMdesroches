191770f0b2e2b2c020ead0a02c28b12282d91d4e
francoisMdesroches <f.deroche@hotmail.com>
2020-09-15 22:50:57 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     if(old_char in string):
-        
+        for i in range(0)
     else:
         return string
 
