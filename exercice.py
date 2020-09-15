d89b3e48542fdd8370d52240dd8d60526b1f14a8
francoisMdesroches <f.deroche@hotmail.com>
2020-09-15 23:05:57 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,7 +24,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 def get_number_of_char(string: str, char: str) -> int:
     nb_Occurences = 0
-    for
+    for i in range(len(string)-1)
 
     pass
 
